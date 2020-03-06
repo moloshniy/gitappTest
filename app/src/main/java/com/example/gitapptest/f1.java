f589970a278b79896ddeefa0817460734e5d8bc7
Viktor Moloshnyi <moloshniyV@gmail.com>
2020-03-06 14:54:02 +0200

    g2

--- app/src/main/java/com/example/gitapptest/f1.java
+++ app/src/main/java/com/example/gitapptest/f1.java
@@ -2,8 +2,8 @@ package com.example.gitapptest;
 
 public class f1 {
     Part1 - Succes part1
-    Part2
-            Part3
-    part4
+    Part2 - success part2
+    Part3 - succes part3
+    part4 - unsuccess part4
             Part5
 }
